f3c1b4062f3ccdf039c3ed957013cdeaf69b8a5d
Rodrigo Leite <rods.leite27@gmail.com>
2021-07-23 05:48:15 -0300

    creating server

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,23 @@
-const port = process.env.PORT || 3001;
-const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.listen(port);
-console.log(`Api rodando na porta ${port}`);
+const app = require('./app');
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(`${__dirname}/public`));
+
+const PORT = process.env.PORT || 3001;
+http.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
+
+
+
