7ae7e4ed31d0cdd1a539dd3372345d018bf20f3c
victorfelipeoliveirarosa <victorfelipeoliveira1@gmail.com>
2021-07-23 16:14:40 -0300

    configuracao inicial servidor back-end

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const socketServer = require('socket.io');
 
 const app = express();
+const path = require('path');
+const http = require('http').createServer(app);
+
+const allowCors = require('../config/cors');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(allowCors);
+
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+const io = socketServer(http, {
+  cors: {
+    origin: 'http://localhost:3000/',
+    methods: ['GET', 'POST', 'PUT'],
+  },
+});
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/test', (req, res) => {
+  res.status(200).json('OK');
+});
 
-module.exports = app;
+module.exports = http;
---
+++ back-end/src/middlewares/error.js
@@ -0,0 +1,7 @@
+const INTERNAL_SERVER_ERROR = 500;
+module.exports = (err, _req, res, _next) => {
+  if (err.err) return res.status(err.err.status).json({ message: err.err.message });
+  res
+    .status(INTERNAL_SERVER_ERROR)
+    .json({ message: `erro no servidor ${err}` });
+};
