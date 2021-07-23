8107c5bee8e0c6bc40d4e56ac9a35c181df6bb3f
Digo-Castro <digo_castro@outlook.com>
2021-07-23 18:37:08 -0300

    [Group 8][Req 1]

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+    <Route exact path="/" component={Follower} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/register" component={Register} />
+    <Route exact path="/customer/products" component={Products} />
+    <Route exact path="/customer/checkout" component={Checkout} />
+    <Route exact path="/customer/orders" component={CustomerOrders} />
+    <Route exact path="/customer/orders/:id" component={OrdersDetails} />
+    <Route exact path="/seller/orders" component={SellerOrders} />
+    <Route exact path="/seller/orders/:id" component={OrdersDetails} />
+    <Route exact path="/confirmation" component={Confirmation} />
+    <Route exact path="/admin/manage" component={Manager} />
+    <Route component={NotFound} />
+  </Switch>
   );
 }
 
---
+++ front-end/src/pages/Follower.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+
+export default function Follower() {
+  return (
+    <Redirect to="/login" />
+  );
+}
