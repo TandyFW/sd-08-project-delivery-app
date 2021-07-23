370d1d86e06486934187745d69cbc6aca849fa38
Daniel <marcio.daniel@msn.com>
2021-07-23 14:50:21 -0300

    Config Simple Server Ok

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 
-// const http = require('http').createServer(app);
+const STATUS_OK = 200;
 
-// const io = require('socket.io')(http, {
-//   cors: {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   },
-// });
+app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static(`${__dirname}/public`));
-
-// require('./sockets/')(io);
-
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/Ping', (_req, res) => res.status(STATUS_OK).json({ message: 'Pong' }));
 
 module.exports = app;
--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Listening in on port: ${PORT}`));
