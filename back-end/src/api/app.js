ed1a19ebf20afeb90d124888d63680ea8714e126
Rodrigo Leite <rods.leite27@gmail.com>
2021-07-23 06:05:06 -0300

    creating server

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(`${__dirname}/public`));
+
+// require('./sockets/')(io);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,23 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = require('./app');
-const http = require('http').createServer(app);
-
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  },
-});
-
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static(`${__dirname}/public`));
 
 const PORT = process.env.PORT || 3001;
-http.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
+
+app.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
 
 
 
