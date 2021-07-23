d8970fc1744fb82c7eef29e49016eeb5a11df79c
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-23 19:30:04 -0300

    corrige express static no app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-app.use('/images', express.useStatic(`${__dirname}/public`));
+app.use('/images', express.static(`${__dirname}/public`));
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
