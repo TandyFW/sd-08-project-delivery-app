f18a40d44517af92e0093decfc9657c6e19855d2
Paulo Simões <paulohbsimoes@gmail.com>
2021-07-24 10:18:49 -0300

    feat(api): add boom errors support to error middleware

--- back-end/src/middlewares/error.js
+++ back-end/src/middlewares/error.js
@@ -1,5 +1,9 @@
 const { STATUS_CODES } = require('http');
 
-module.exports = (_err, _req, res, _next) => {
+module.exports = (err, _req, res, _next) => {
+  if (err.isBoom) {
+    const { statusCode, message } = err.output.payload;
+    return res.status(statusCode).json({ message });
+  }
   res.status(500).json({ message: STATUS_CODES[500] });
 };
