530dc257983a3375b5cd8d85bfba804a5b77233a
Paulo Simões <paulohbsimoes@gmail.com>
2021-07-24 11:02:19 -0300

    feat(api): finish login controller

--- back-end/src/controllers/login.js
+++ back-end/src/controllers/login.js
@@ -1,16 +1,45 @@
+const rescue = require('express-rescue');
 const joi = require('joi');
+const boom = require('@hapi/boom');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const fs = require('fs');
+const UserService = require('../services/user');
+const md5 = require('../utils/md5');
+
+const secret = fs.readFileSync(
+  path.resolve(__dirname, '..', '..', 'jwt.evaluation.key'),
+);
 
 const loginSchema = joi.object({
   email: joi.string().required(),
   password: joi.string().required(),
 });
 
-const login = (req, res) => {
+const getJwtToken = (payload) => {
+  const jwtConfig = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secret, jwtConfig);
+};
+
+const login = rescue(async (req, res) => {
   const { error } = loginSchema.validate(req.body);
 
   if (error) throw error;
 
-  res.json({ message: 'okay' });
-};
+  const { email, password } = req.body;
+
+  const user = await UserService.findByEmail(email);
+
+  if (!user || user.password !== md5(password)) {
+    throw boom.unauthorized('Invalid user');
+  }
+
+  const token = getJwtToken({ id: user.id, email });
+
+  res.json({ token });
+});
 
 module.exports = login;
