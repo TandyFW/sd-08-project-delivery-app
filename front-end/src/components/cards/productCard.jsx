4d18a95ba66bc8e3c5029cb416b325fdb0c25124
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-07-26 19:45:22 -0300

    Products page started

---
+++ front-end/src/components/cards/productCard.jsx
@@ -0,0 +1,12 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+function productCard() {
+  return (
+    <Grid item xs={ 12 } s={ 6 } m={ 4 } l={ 3 }>
+      <p>Card</p>
+    </Grid>
+  );
+}
+
+export default productCard;
--- front-end/src/pages/client/Products.jsx
+++ front-end/src/pages/client/Products.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 
+import NavBar from '../../components/NavBar';
+
+const headerButtons = [
+  {
+    label: 'PRODUTOS',
+    href: '/customer/products',
+  },
+  {
+    label: 'MEUS PEDIDOS',
+    href: 'customer/orders',
+  },
+];
+
+// const products = [
+//   {
+//     name: 'Skol',
+//     price: '7.50',
+//     amount: '330ml',
+//     imagePath: 'https://superprix.vteximg.com.br/arquivos/ids/180544-292-292/385b999ce16bad393a31dfee6fa12c4c_cerveja-skol-473ml--lata-_lett_1.jpg',
+//   },
+//   {
+//     name: 'Antarctica',
+//     price: '7.50',
+//     amount: '500ml',
+//     imagePath: 'https://superprix.vteximg.com.br/arquivos/ids/180713-292-292/5e5c08c4a8071d0d6983b4aba2ea8e8c_cerveja-antarctica-473ml--lata-_lett_1.jpg',
+//   },
+//   {
+//     name: 'Heineken',
+//     price: '7.50',
+//     amount: '500ml',
+//     imagePath: 'https://santaluzia.vteximg.com.br/arquivos/ids/963474-1000-1000/1156454.jpg',
+//   },
+// ];
+
 function Products() {
   return (
-    <div><p>Products</p></div>
+    <>
+      <NavBar headerButtons={ headerButtons } />
+      <div><p>Products</p></div>
+
+    </>
   );
 }
 
