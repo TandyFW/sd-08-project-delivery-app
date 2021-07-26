85091e76cb1b4a3756528cee7fece4beb88001cb
Daniel <marcio.daniel@msn.com>
2021-07-26 17:47:41 -0300

    req 1,2,3

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -3,24 +3,25 @@ const createToken = require('../auth/createToken');
 const loginSchema = require('../schemas/loginSchema');
 const clientError = require('../utils/clientError');
 
-let dataloginDB = null;
+let dataLoginDB = null;
 
-const login = async (dataForlogin) => {
-  const { error } = loginSchema.login.validate(dataForlogin);
+const login = async (dataForLogin) => {
+  const { error } = loginSchema.login.validate(dataForLogin);
+  console.log(dataForLogin);
   if (error) return clientError.badRequest(error.details[0].message);
 
   try {
-  const { dataValues } = await registerServices.getByEmail(dataForlogin.email);
-  dataloginDB = dataValues;
+  const { dataValues } = await registerServices.getByEmail(dataForLogin.email);
+  dataLoginDB = dataValues;
   } catch (err) {
     return clientError.badRequest('User Not registered');
   }
   
-  if (dataloginDB.password !== dataForlogin.password) {
+  if (dataLoginDB.password !== dataForLogin.password) {
     return clientError.badRequest('Email Our Password Invalid');
   }
   
-  const { name, email, id } = dataloginDB;
+  const { name, email, id } = dataLoginDB;
    const token = await createToken({ name, email, id });
    return token;
 };
--- front-end/src/services/index.js
+++ front-end/src/services/index.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 const URL_BASE = 'http://localhost:3001';
 
 export async function getAllUsers() {
-  const users = await axios.get(`${URL_BASE}/users`)
+  const users = await axios.get(`${URL_BASE}/register`)
     .then((response) => response.data);
   return users;
 }
 
-export async function getProducts() {
-  const products = await axios.get(`${URL_BASE}/customer/products`)
+export async function getAllProducts() {
+  const products = await axios.get(`${URL_BASE}/products`)
     .then((response) => response.data);
   return products;
 }
 
 export async function create(name, email, password, role) {
   try {
-    const user = await axios.post(`${URL_BASE}/users`,
+    const user = await axios.post(`${URL_BASE}/register`,
       { name, email, password, role })
       .then((response) => response.data);
     return user;
