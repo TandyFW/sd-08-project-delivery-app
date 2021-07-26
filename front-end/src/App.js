29a2aa80b5052f122d8f953e1804c7efe4773f01
Aline Debastiani <debastianialine@gmail.com>
2021-07-26 18:01:55 -0300

    criando a pagina de registros

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/login" component={ Login } />
+      <Route path="/register" component={ Register } />
       <Redirect from="/" to="/login" />
     </Switch>
   );
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormControl, InputLabel, Button } from '@material-ui/core';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div><p>Register</p></div>
+    <>
+      <h3>Cadastro</h3>
+      <FormControl>
+        <InputLabel htmlFor="name-input">Nome</InputLabel>
+        <input
+          id="name-input"
+          type="text"
+          placeholder="Digite seu nome"
+          data-testid="common_register__input-name"
+          value={ name }
+          onChange={ ({ target }) => setName(target.value) }
+        />
+        <InputLabel htmlFor="email-input">Email</InputLabel>
+        <input
+          id="email-input"
+          type="email"
+          placeholder="Digite seu email"
+          data-testid="common_register__input-email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+        <InputLabel htmlFor="password-input">Senha</InputLabel>
+        <input
+          id="password-input"
+          type="password"
+          placeholder="Escolha uma senha"
+          data-testid="common_register__input-password"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+        <Button
+          data-testid="common_register__button-register"
+          type="submit"
+        >
+          CADASTRAR
+        </Button>
+      </FormControl>
+    </>
   );
 }
 
