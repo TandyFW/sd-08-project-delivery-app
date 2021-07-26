a67ffe2cee34e9b568bccb072827a2f6bd7b9d3d
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-26 19:31:45 -0300

    cria o AuthController no backend

---
+++ back-end/src/controllers/AuthController.js
@@ -0,0 +1,18 @@
+
+module.export = async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token || token === '') {
+    return res.status(403).json({ message: 'Token not found' });
+  }
+  try {
+    const decoded = jwt.verify(token, secret);
+    const user = await User.findOne({ where: { email: decoded.data.email } });
+    if (!user) {
+      return res.status(403).json({ message: 'Expired or invalid token' });
+    }
+    req.user = decoded.data;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Expired or invalid token' });
+  }
+}
--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
     <div>
-      
+      <span data></span>
+      <img data-testid={`customer_products__img-card-bg-image-${product.id}`}/>
     </div>
   )
 }
