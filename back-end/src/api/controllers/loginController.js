001d37df4a75be29ef30167fc0cab9a8ffb6c49e
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-07-26 15:03:00 -0300

    Acerto lint.

--- back-end/src/api/controllers/loginController.js
+++ back-end/src/api/controllers/loginController.js
@@ -2,7 +2,7 @@ const userLogin = async (req, res) => {
   const { token } = req;
   const { name, email, role } = req.body;
 
-  return res.status(200).json({ token, user: { name, email, role }});
+  return res.status(200).json({ token, user: { name, email, role } });
 };
 
 module.exports = { userLogin };
