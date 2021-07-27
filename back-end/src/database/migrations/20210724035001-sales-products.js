86a4ea47626dbf1fdb4af3a99a8c7a0c9dd9cc14
Paulo Simões <paulohbsimoes@gmail.com>
2021-07-27 19:07:33 -0300

    feat(api): add junction table sales-products migration

---
+++ back-end/src/database/migrations/20210724035001-sales-products.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('sales_products', {
+      saleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'sales',
+          key: 'id'
+        },
+        field: 'sale_id'
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'products',
+          key: 'id'
+        },
+        field: 'product_id'
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      }
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('sales_products');
+  }
+};
