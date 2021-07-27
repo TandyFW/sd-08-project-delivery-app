5fefbca67b8d198d4514f9d322d876615ac74978
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-27 10:33:25 -0300

    adciona elementos e data-testids ao componente ProductCard

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const ProductCard = ({product}) => {
+  const { id, name, price, url_image } = product;
   return (
-    <div>
-      <span data></span>
-      <img data-testid={`customer_products__img-card-bg-image-${product.id}`}/>
-    </div>
+    <section className="card">
+      <span data-testid={`customer_products__element-card-price-${id}`}>{`R$ {price}`}</span>
+      <img data-testid={`customer_products__img-card-bg-image-${id}`} src={url_image}/>
+      <div>
+        <p data-testid={`customer_products__element-card-title-${name}`}>{name}</p>
+      </div>
+      <div>
+        <button data-testid={`customer_products__button-card-rm-item-${id}`} type="button" value="-" />
+        <span data-testid={`customer_products__input-card-quantity-${id}`}>0</span>
+        <button data-testid={`customer_products__button-card-add-item-${id}`} type="button" value="+" />
+      </div>
+    </section>
   )
 }
 
