2e2e485b280523e07e220e380bccae7097aa89f6
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-27 11:30:51 -0300

    corrige AuthController, rota /customer/products retornando corretamente os produtos

--- back-end/src/controllers/AuthController.js
+++ back-end/src/controllers/AuthController.js
@@ -1,19 +1,24 @@
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const { findByEmail } = require('../services/UserService')
 
-module.export = async (req, res) => {
+module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token || token === '') {
     return res.status(403).json({ message: 'Token not found' });
   }
   try {
-    const secret = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
+    const secret = fs.readFileSync(`${__dirname}/../../jwt.evaluation.key`, 'utf-8');
     const decoded = jwt.verify(token, secret);
-    const user = await User.findOne({ where: { email: decoded.data.email } });
+    console.log('decoded: ', decoded)
+    const user = await findByEmail(decoded.data.email);
     if (!user) {
       return res.status(403).json({ message: 'Expired or invalid token' });
     }
     req.user = decoded.data;
     next();
   } catch (error) {
-    return res.status(403).json({ message: 'Expired or invalid token' });
+    console.log('error:', error)
+    return res.status(403).json({ message: error });
   }
 }
