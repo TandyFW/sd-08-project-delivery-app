9f68c46d5c089a6bc581e4745b9a3a784031a81d
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-07-27 13:38:58 -0300

    Continuing Products page

--- front-end/src/components/cards/productCard.jsx
+++ front-end/src/components/cards/productCard.jsx
@@ -1,10 +1,31 @@
-import { Grid } from '@material-ui/core';
+import { Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography } from '@material-ui/core';
 import React from 'react';
 
 function ProductCard() {
   return (
     <Grid item xs={ 12 } s={ 6 } m={ 4 } l={ 3 }>
-      <p>Card</p>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={ name }
+            height="140"
+            image={ imagePath }
+            title={ name }
+          />
+          <CardContent>
+            <Typography>
+              { name }
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
     </Grid>
   );
 }
