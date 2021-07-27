d2966fd7f2bfb5b8fac2e3fde5d4418a9ba5d93e
Victor-WLS2 <victorfelipeoliveira1@gmail.com>
2021-07-27 15:31:30 -0300

    endpoint products

---
+++ back-end/src/services/productService.js
@@ -0,0 +1,12 @@
+const { Product } = require('../database/models');
+const productsError = require('../schema/productsError');
+
+const getAllProducts = async () => {
+  const products = await Product.findAll();
+  if (!products) return productsError.productsNotFound;
+  return products;
+};
+
+module.exports = {
+  getAllProducts,
+}
