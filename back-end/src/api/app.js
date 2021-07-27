b8b49f19df322102eb9618634f5e471c30a5eff3
jnpefo <jnpefo@gmail.com>
2021-07-27 17:47:40 -0300

    Feito o find dos produtos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const loginController = require('../controllers/loginController');
 const registerController = require('../controllers/registerController');
+const path = require('path');
+const getProducts = require('../controllers/getProducts');
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.use('/images', express.static(path.join(__dirname, '../../', 'public')));
 
+app.get('/products', getProducts)
 app.post('/login', loginController);
 app.post('/register', registerController);
 
---
+++ back-end/src/services/productsService.js
@@ -0,0 +1,21 @@
+const { products } = require('../database/models');
+
+
+const findProducts = async () => {
+  try {
+    const result = await products.findAll({});
+    return {
+      statusCode: 200,
+      json: { result },
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      message: 'Erro in DB',
+    };
+  }
+};
+
+module.exports = {
+  findProducts,
+};
