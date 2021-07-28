4c1715479b9e880ae0014845639f5b4601732e8f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-28 19:49:49 -0300

    cria CustomerOrderCart(componente) e CustomerOrders(page) - esboços

---
+++ front-end/src/components/CustomerOrderCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomerOrderCard = () => {
+  return (
+    <div>
+      <h1>Card de pedido do cliente</h1>
+    </div>
+  )
+}
+
+export default CustomerOrderCard
---
+++ front-end/src/pages/CustomerOrders.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerOrders = () => {
+  return (
+    <div>
+      <h1>Pagina de pedidos do cliente</h1>
+    </div>
+  )
+}
