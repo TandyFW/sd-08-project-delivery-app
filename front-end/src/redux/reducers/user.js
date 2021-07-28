f8677ec7df30b9ff2b7514e32070fbd7e1675e8f
hugobrr <hugobrr@gmail.com>
2021-07-28 19:17:17 -0300

    ajuda do rodrigo

---
+++ front-end/src/redux/reducers/user.js
@@ -0,0 +1,16 @@
+import { USER } from '../actions';
+
+const INITIAL_STATE = {
+  user: [{}],
+};
+
+export default function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER:
+    return {
+      ...state, user: action.array,
+    };
+  default:
+    return state;
+  }
+}
