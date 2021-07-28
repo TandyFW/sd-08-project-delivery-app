bbc72535f6685e5c08c2644d8ad19ae9c4947ea6
Ediberto Oliveira <edibertooliveira@aol.com>
2021-07-28 19:52:26 -0300

    [Req-26]  -   seller route conenct frontend  backend

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -4,30 +4,33 @@ import { Col, Container, Row } from 'react-bootstrap';
 import OrderCard from '../components/OrderCard';
 import useAxios from '../hooks/useAxios';
 import { API } from '../service/backendApi';
-// seller_orders__element-card-address-<id></id>
+
 function Orders() {
   const [, route] = useLocation().pathname.split('/');
+  const hasSeller = route === 'seller';
   const { request, response } = useAxios();
   useEffect(() => request(API[route]), [request, route]);
   return (
     <Container>
       <Row xs={ 4 } md={ 2 } className="p-4">
-        {
-          response && response.data.map((variant, idx) => (
+        {response
+          && response.data.map((variant, idx) => (
             <Link key={ idx } to={ `/${route}/orders/${variant.id}` }>
               <OrderCard requests={ variant } user={ route }>
-                <Col
-                  data-testid={
-                    `${route}_orders__element-card-address-${variant.id}`
-                  }
-                  className="text-center p-4"
-                >
-                  { variant.id }
-                </Col>
+                {hasSeller && (
+                  <Row
+                    data-testid={
+                      `${route}_orders__element-card-address-${variant.address}`
+                    }
+                  >
+                    <Col className="text-center p-4">
+                      {variant.deliveryAddress}
+                    </Col>
+                  </Row>
+                )}
               </OrderCard>
             </Link>
-          ))
-        }
+          ))}
       </Row>
     </Container>
   );
