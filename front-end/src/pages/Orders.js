c67cf6f88f498803bca735aeefdf2bb5a1cb5150
Ediberto Oliveira <edibertooliveira@aol.com>
2021-07-28 17:57:39 -0300

    [Req-26]  -  Optimized the route seller's customer order

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,20 +1,30 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Row } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
 import OrderCard from '../components/OrderCard';
 import useAxios from '../hooks/useAxios';
-import { API_CUSTUMER_ORDER_URL } from '../service/backendApi';
-
+import { API } from '../service/backendApi';
+// seller_orders__element-card-address-<id></id>
 function Orders() {
+  const [, route] = useLocation().pathname.split('/');
   const { request, response } = useAxios();
-  useEffect(() => request(API_CUSTUMER_ORDER_URL), [request]);
+  useEffect(() => request(API[route]), [request, route]);
   return (
     <Container>
       <Row xs={ 4 } md={ 2 } className="p-4">
         {
           response && response.data.map((variant, idx) => (
-            <Link key={ idx } to={ `/customer/orders/${variant.id}` }>
-              <OrderCard requests={ variant } user="customer" />
+            <Link key={ idx } to={ `/${route}/orders/${variant.id}` }>
+              <OrderCard requests={ variant } user={ route }>
+                <Col
+                  data-testid={
+                    `${route}_orders__element-card-address-${variant.id}`
+                  }
+                  className="text-center p-4"
+                >
+                  { variant.id }
+                </Col>
+              </OrderCard>
             </Link>
           ))
         }
