3c3745bde26eee60abeb49b57e9bdda16206a040
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-28 20:06:06 -0300

    corrige erros de formatação

--- front-end/src/components/CustomerOrderCard.js
+++ front-end/src/components/CustomerOrderCard.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const CustomerOrderCard = () => {
-  return (
+const CustomerOrderCard = () => (
     <div>
       <h1>Card de pedido do cliente</h1>
     </div>
-  )
-}
+  );
 
 export default CustomerOrderCard
