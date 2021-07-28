11f2af3db711e81dfbff9b6bed286c626a67c331
Clênio Galvão <clenio.cgm@gmail.com>
2021-07-28 18:51:40 -0300

    Criando os cards dos produtos

--- front-end/src/views/Customer/Products/index.js
+++ front-end/src/views/Customer/Products/index.js
@@ -1,16 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import NavBar from '../../Components/NavBar';
+import './styles.css';
 
 const Products = () => {
   const user = { name: 'Clênio', id: 1 }; // vem do estado global
   const [products, setProducts] = useState([]);
 
+  async function getData() {
+    const myInit = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+    await fetch('http://localhost:3001/customer/products', myInit)
+      .then((response) => response.json())
+      .then((data) => console.log(data.products) || setProducts(data.products))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <>
+    <div className="conteiner">
       <NavBar userType="client" userName={ user.name } />
-      <div>os produtos vão aqui</div>
-    </>
+      {products.length > 0 && (
+        products.map((product) => (
+          <div key={ product.id }>
+            <div className="card">
+              <div>{product.price}</div>
+              <div className="card_img">
+                <img
+                  src={ product.url_image }
+                  alt="produto"
+                />
+              </div>
+              <div className="card_body">
+                <h6><b>{product.name}</b></h6>
+              </div>
+            </div>
+          </div>
+        )))}
+    </div>
   );
 };
 
