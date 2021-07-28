64ad6a09706cd987b114759277cf9390ee097ff7
hugobrr <hugobrr@gmail.com>
2021-07-28 20:34:35 -0300

    Me ajuda do rodrigo

--- front-end/src/components/UserTable.js
+++ front-end/src/components/UserTable.js
@@ -4,53 +4,64 @@ import PropTypes from 'prop-types';
 import { getAllUsers, exclude } from '../services';
 import { userAction } from '../redux/actions';
 
+function funcaoBLa(users) {
+  // const { stateUsers } = this.props;
+  this.setState(users);
+}
 class UserTable extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      users: [{}],
+    };
   }
 
   async componentDidMount() {
     const { dispatchTable } = this.props;
-    const users = await getAllUsers();
-
+    const users = await getAllUsers() || [{}];
+    funcaoBLa(users.registers);
     dispatchTable(users);
   }
 
-  refresh() {
-    // re-renders the component
-    this.setState({});
-  }
+  // refresh() {
+  //   // re-renders the component
+  //   this.setState({});
+  // }
 
   async exclude(id) {
+    const { users } = this.state;
     const result = await exclude(id);
-    setState({});
+    const results = users.filter((item) => item.id !== id);
+    setState({ user: results });
     console.log(result);
   }
 
   render() {
+    // const {users!
     // const { history } = this.props;
     // console.log(history);
-    const { stateUsers } = this.props;
-    console.log(stateUsers);
-    // const tableUser = stateUsers.user || [{}];
-    // const keyOfstatateUsers = Object.keys(tableUser[0]);
+    // const { stateUsers } = this.props;
+    const { users } = this.state;
+    // console.log(stateUsers.registers);
+    // const tableUser = stateUsers.registers || [{}];
+    const keyOfstatateUsers = Object.keys(users[0]);
     // keyOfstatateUsers.push('Excluir');
     return (
       <div className="cardlist-container">
         <table>
           <thead>
             <tr>
-              {/* { keyOfstatateUsers
+              { keyOfstatateUsers
             && keyOfstatateUsers.map((title) => (
               <th key={ title }>{title}</th>
-            ))} */}
+            ))}
+
               <th key="excluir">Excluir</th>
             </tr>
           </thead>
           <tbody>
 
-            { tableUser && tableUser.map((user, index) => (
+            { users && users.map((user, index) => (
               <tr
                 id={ `${index}` }
                 className="user"
@@ -82,13 +93,13 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = (state) => ({
-  stateUsers: state.user.user,
+  stateUsers: state.userReducer.user,
 });
 
 UserTable.propTypes = {
   // history: PropTypes.shape().isRequired,
   dispatchTable: PropTypes.func.isRequired,
-  stateUsers: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // stateUsers: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserTable);
