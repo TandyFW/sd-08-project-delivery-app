ff5ce3580b67383e39180456a190da5b744a5c2f
amandaos <amanda.775.souza@gmail.com>
2021-07-28 11:34:40 -0300

    fix: corrige lint

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 const port = process.env.PORT || 3001;
 const app = require('./app');
 const userController = require('../controllers/userController');
