c4036f6f5825f54dcf9f1a46e9755b8495064627
Ediberto Oliveira <edibertooliveira@aol.com>
2021-07-28 17:02:08 -0300

    [Req-00]  - token integrated with user registry

--- back-end/src/routes/registerRouter.js
+++ back-end/src/routes/registerRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { celebrate, Joi, Segments } = require('celebrate');
+const auth = require('../api/middlewares/isAuthenticated');
 const registerController = require('../controllers/registerController');
 
 const registerRouter = Router();
