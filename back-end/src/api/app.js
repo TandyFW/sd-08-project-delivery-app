d95cf8998203543d74b85d163b5707b3823bcd98
Paulo Simões <paulohbsimoes@gmail.com>
2021-07-29 16:59:42 -0300

    feat(api): add public folder middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const middlewares = require('../middlewares');
 const routes = require('../routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 
 app.use(cors({
   origin: '*',
