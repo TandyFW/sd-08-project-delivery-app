9dcec9fe9bc8dbc75e426d0961bcabcbea3172ff
enicio <jessandro42@gmail.com>
2021-07-28 23:06:10 -0300

    Tabela de detalhes estruturada

--- front-end/src/views/Components.js
+++ front-end/src/views/Components.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import OrderDetailsCard from '../components/orderDetailsCrad/orderDetailsCard';
+import SellerTable from '../components/sellerTable/sellerTable';
 
 function Components() {
   return (
     <div>
       <OrderDetailsCard />
+      <SellerTable />
     </div>
   );
 }
