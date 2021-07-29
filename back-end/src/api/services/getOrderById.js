c236f49e83a888bf1e7ba4489e20949b959bd6f6
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-07-29 15:54:39 -0300

    Refatora para resultado detalhado da busca de venda por saleId

--- back-end/src/api/services/getOrderById.js
+++ back-end/src/api/services/getOrderById.js
@@ -1,5 +1,5 @@
 const { Op } = require('sequelize');
-const { sale } = require('../../database/models');
+const { sale, product, salesProduct } = require('../../database/models');
 
 module.exports = async (id, role, saleId) => {
   let userId = '';
@@ -11,7 +11,14 @@ module.exports = async (id, role, saleId) => {
   const result = await sale.findAll({
     where: { [Op.and]: [
         { id: saleId },
-        { [Op.or]: [{ sellerId }, { userId }] }] },
+      { [Op.or]: [{ sellerId }, { userId }] }],
+    },
+    include: { model: salesProduct,
+        attributes: ['quantity'],
+        include: { model: product,
+          atributes: ['name', 'price', 'url_image'],
+        },
+      },
   });
 
   return result;
