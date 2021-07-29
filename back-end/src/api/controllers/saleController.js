a616d875d45d6f49b945fe6897783f804ac6234f
Tandy <tandyfw@gmail.com>
2021-07-29 09:30:36 -0300

    lint hotfix

--- back-end/src/api/controllers/saleController.js
+++ back-end/src/api/controllers/saleController.js
@@ -1,5 +1,5 @@
 const { getAll, create } = require('../services/saleService');
-const { getUserByName, getUserByEmail } = require('../services/userService');
+const { getUserByEmail } = require('../services/userService');
 const { OK, CREATED } = require('../services/statusCode');
 
 const getAllSales = async (req, res) => {
@@ -11,7 +11,6 @@ const createSales = async (req, res) => {
   const email = req.decoded;
   const { seller, ...data } = req.body;
   const User = await getUserByEmail(email);
-  // const Seller = await getUserByName(seller);
   const saleCreated = await create({ ...data, userId: User.id, status: 'Pendente' });
   res.status(CREATED).json(saleCreated);
 };
