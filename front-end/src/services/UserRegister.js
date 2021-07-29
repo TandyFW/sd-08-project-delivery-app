b32aa3115a637909eb639879901fd6a142c63852
Victor Miwa <victoryukimiwa@hotmail.com>
2021-07-29 16:41:03 -0300

    revendo 02 tela de registro

--- front-end/src/services/UserRegister.js
+++ front-end/src/services/UserRegister.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
 
 const UserRegister = async (name, email, password) => {
-  axios.post('http://localhost:3001/register', {
-    name,
-    email,
-    password,
-  })
-    .then((response) => {
-      console.log(response);
-      if (response.message === 'Created') return true;
-    })
-    .catch((err) => console.log(err));
-  return false;
+  try {
+    const result = await axios.post('http://localhost:3001/register', {
+      name,
+      email,
+      password,
+    });
+
+    if (result.message === 'Created') return true;
+
+    return false;
+  } catch (error) {
+    console.log(err);
+  }
 };
 
 export default UserRegister;
