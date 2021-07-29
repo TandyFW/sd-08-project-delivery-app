0be1bbc71cf6b0403459e3a7ad1fe33ddac698b1
caiomzrocha <caiomzrocha@gmail.com>
2021-07-29 10:09:27 -0300

    [REQ-17-21] Insere uma nova venda e os realcionamentos entre produto e venda.

--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -1,9 +1,17 @@
-const { sales } = require('../database/models');
+const { sales, salesProducts } = require('../database/models');
 
-// totalPrice, deliveryAddress, deliveryNumber, salesDate, status
 const createSale = async (data) => {
-  const { dataValues } = await sales.create(data);
-  return dataValues;
+  const { productList, ...saleData } = data;
+  const { dataValues } = await sales.create(saleData);
+  const saleId = dataValues.id;
+
+  productList.forEach(async (product) => {
+    const { id, quantity } = product;
+    const saleProductData = { saleId, productId: id, quantity };
+    await salesProducts.create(saleProductData);
+  });
+
+  return { ...dataValues, productList };
 };
 
 module.exports = {
