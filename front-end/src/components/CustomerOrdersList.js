2f37523026a01b0ae4512d34e490497de4db4800
Thiago Ferreira <thiagocristhian@gmail.com>
2021-07-29 16:45:46 -0300

    contruindo customer card

--- front-end/src/components/CustomerOrdersList.js
+++ front-end/src/components/CustomerOrdersList.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
+const date = new Date();
+const formatedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 const orders = [
-  {
-    orderId: 1,
-    status: 'pendente',
-    key1: 'teste',
-    key1: 'teste',
-    key1: 'teste',
-  }
-]
+  { order_id: 1, status: 'pendente', date: formatedDate, price: 24.65 },
+  { order_id: 2, status: 'preparando', date: formatedDate, price: 88.98 },
+  { order_id: 3, status: 'entregue', date: formatedDate, price: 25.30 },
+];
 
 class CardList extends React.Component {
   render() {
@@ -19,7 +17,23 @@ class CardList extends React.Component {
     return (
       <div className="cardlist-container">
         {
-          
+          orders.map((order, i) => (
+            <div key={ i } className="orderlist-container">
+              <div>
+                <h4>Pedido</h4>
+                {order.order_id}
+              </div>
+              <div
+                className={ order.status }
+              >
+                { order.status }
+              </div>
+              <div>
+                <div>{ order.date }</div>
+                <div>{ `R$ ${order.price}` }</div>
+              </div>
+            </div>
+          ))
         }
       </div>
     );
