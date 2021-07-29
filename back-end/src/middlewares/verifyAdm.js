621e3b710490fd2d845dd028ab9fe0f926cc9b22
hugobrr <hugobrr@gmail.com>
2021-07-29 07:02:54 -0300

    lint2

--- back-end/src/middlewares/verifyAdm.js
+++ back-end/src/middlewares/verifyAdm.js
@@ -3,7 +3,7 @@ const Boom = require('@hapi/boom');
 
 const verifyAdm = rescue(async (req, _res, next) => {
   const { role } = req.dataUser;
-  if(role !== 'administrator') return Boom.unauthorized('Usuario não autorizado');
+  if (role !== 'administrator') return Boom.unauthorized('Usuario não autorizado');
   next();
 });
 module.exports = verifyAdm;
