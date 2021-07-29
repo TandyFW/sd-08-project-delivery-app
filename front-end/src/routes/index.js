e823ef014fd8217544b65b231f2027a143208d24
Paulo Simões <paulohbsimoes@gmail.com>
2021-07-29 17:09:05 -0300

    feat(api): add Checkout route

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Products from '../pages/Products';
+import Checkout from '../pages/Checkout';
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Routes = () => (
       <Route path="/login" component={ Login } />
       <Route path="/register" component={ Register } />
       <Route path="/customer/products" component={ Products } />
+      <Route path="/customer/checkout" component={ Checkout } />
       <Route exact path="/" render={ (props) => <Redirect { ...props } to="login" /> } />
     </Switch>
   </BrowserRouter>
