b9e635376e8a9182c34c7ce3db055473648d2e31
Thiago Ferreira <thiagocristhian@gmail.com>
2021-07-29 16:19:11 -0300

    iniciando Requisitos 22 a 24

---
+++ front-end/src/components/CustomerOrdersList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const orders = [
+  { teste: 'teste' }
+]
+
+class CardList extends React.Component {
+  render() {
+    // const { history } = this.props;
+    // console.log(history);
+    // const { stateProducts } = this.props;
+    // const { state } = this;
+    return (
+      <div className="cardlist-container">
+        {
+          
+        }
+      </div>
+    );
+  }
+}
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CardList);
+export default CardList;
