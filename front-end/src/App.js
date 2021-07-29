bceee4184dc3a8837f643b348ae30a146ca5772a
Digo-Castro <digo_castro@outlook.com>
2021-07-29 16:18:25 -0300

    [Group 8][Reqs 22-24 e 27-29] - Orders page - customer/seller

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Checkout from './pages/Checkout';
 import Follower from './pages/Follower';
 import Login from './pages/Login';
+import Orders from './pages/Orders';
 import Register from './pages/Register';
-import Checkout from './pages/Checkout';
 import './App.css';
 
 function App() {
@@ -14,9 +15,9 @@ function App() {
       <Route exact path="/register" component={ Register } />
       {/* <Route exact path="/customer/products" component={Products} /> */}
       <Route exact path="/customer/checkout" component={ Checkout } />
-      {/* <Route exact path="/customer/orders" component={CustomerOrders} /> */}
+      <Route exact path="/customer/orders" component={ Orders } />
       {/* <Route exact path="/customer/orders/:id" component={OrdersDetails} /> */}
-      {/* <Route exact path="/seller/orders" component={SellerOrders} /> */}
+      <Route exact path="/seller/orders" component={ Orders } />
       {/* <Route exact path="/seller/orders/:id" component={OrdersDetails} /> */}
       {/* <Route exact path="/confirmation" component={Confirmation} /> */}
       {/* <Route exact path="/admin/manage" component={Manager} /> */}
