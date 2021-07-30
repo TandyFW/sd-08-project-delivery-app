9698603e78e3890229897adbf1cb8a348d019671
Seu nome <luiza.dilly@gmail.com>
2021-07-30 16:57:30 -0300

    req 27

--- front-end/src/pages/Seller.jsx
+++ front-end/src/pages/Seller.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Seller = () => (
-  <div>Dilly Dillu!</div>
-);
+import NavBar from '../components/NavBar';
+
+const Seller = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const prefix = 'seller_orders__element';
+  const [sale, setSale] = useState([]);
+
+  const fetchApi = async () => {
+    const { data } = await axios.get('http://localhost:3001/delivery/sellersales', {
+      headers: {
+        authorization: user.token,
+      } });
+    setSale(data);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, [fetchApi]);
+
+  return (
+    <>
+      <NavBar user={ user.name } />
+      {
+        sale.map((infos) => (
+          <>
+            <span data-testid={ `${prefix}-order-id-${infos.id}` }>
+              { infos.id }
+            </span>
+            <span data-testid={ `${prefix}-delivery-status-${infos.id}` }>
+              { infos.status }
+            </span>
+            <span data-testid={ `${prefix}-card-price-${infos.id}` }>
+              {
+                infos.total_price
+              }
+            </span>
+            <span data-testid={ `${prefix}-card-address-${infos.id}` }>
+              { infos.delivery_address }
+            </span>
+            <span>{ infos.delivery_number }</span>
+            <span data-testid={ `${prefix}-order-date-${infos.id}` }>
+              { infos.sale_date }
+            </span>
+          </>
+        ))
+      }
+    </>
+  );
+};
 
 export default Seller;
