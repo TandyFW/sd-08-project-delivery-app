c4e239a3b567e1b213ed2806795751d8d293c773
Ediberto Oliveira <edibertooliveira@aol.com>
2021-07-30 19:58:28 -0300

    [Req-00]  -  header companent applying logic to other tests

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useLocation } from 'react-router-dom';
 
 export default function Header() {
   const [redirect, setRedirect] = useState(false);
+  const [, role, route] = useLocation().pathname.split('/');
   if (redirect) return <Redirect to="/login" />;
 
   const obj = localStorage.getItem('user');
@@ -13,7 +14,7 @@ export default function Header() {
     setRedirect(true);
   };
 
-  const { name, role } = JSON.parse(obj) || 'undefined';
+  const { name } = JSON.parse(obj) || 'undefined';
   const routers = {
     customer: [
       { name: 'PRODUTOS', link: '/customer/products' },
@@ -32,20 +33,20 @@ export default function Header() {
             <Nav.Link
               key={ idx }
               href={ contents.link }
-              data-testid={ `${role}_products__element-navbar-link-${
+              data-testid={ `${role}_${route}__element-navbar-link-${
                 contents.link.split('/')[1]
               }` }
             >
               { contents.name }
             </Nav.Link>
           ))}
-          <Navbar.Text data-testid="customer_products__element-navbar-user-full-name">
+          <Navbar.Text data-testid={ `${role}_${route}__element-navbar-user-full-name` }>
             {name}
           </Navbar.Text>
           <Button
             variant="danger"
             onClick={ logout }
-            data-testid="customer_products__element-navbar-link-logout"
+            data-testid={ `${role}_${route}__element-navbar-link-logout` }
           >
             Sair
           </Button>
