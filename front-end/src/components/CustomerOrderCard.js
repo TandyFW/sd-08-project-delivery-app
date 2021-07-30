10273ef7421ff941cad5d0f7a83f0ff4fa6809e4
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-30 17:49:12 -0300

    corrige mock das orders e cria cards com data-testids corretos

--- front-end/src/components/CustomerOrderCard.js
+++ front-end/src/components/CustomerOrderCard.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './CustomerOrderCard.css';
 
-const CustomerOrderCard = () => (
-    <div>
-      <h1>Card de pedido do cliente</h1>
+const CustomerOrderCard = ({ order }) => (
+  <div className="order-card">
+    <div className="order-card-number">
+      <p>Pedido</p>
+      <p data-testid={ `customer_orders__element-order-id-${order.id}` }>{order.id}</p>
     </div>
-  );
+    <div className="order-card-status">
+      <span
+        data-testid={ `customer_orders__element-delivery-status-${order.id}` }
+      >
+        {order.status}
+      </span>
+    </div>
+    <div className="order-card-info">
+      <p
+        data-testid={ `customer_orders__element-order-date-${order.id}` }
+      >
+        {order.date}
+      </p>
+      <p
+        data-testid={ `customer_orders__element-card-price-${order.id}` }
+      >
+        {order.total}
+      </p>
+    </div>
+  </div>
+);
 
-export default CustomerOrderCard
+CustomerOrderCard.propTypes = {
+  order: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    total: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default CustomerOrderCard;
--- front-end/src/pages/CustomerOrders.js
+++ front-end/src/pages/CustomerOrders.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import CustomerOrderCard from '../components/CustomerOrderCard';
 
 const MOCK_ORDERS = [
   {
     id: 1,
-    user_id: 03,
-    seller_id: 02,
-    delivery_address: 'pendente',
-    unityPrice: '2,49',
-    subTotal: '9,96',
+    status: 'pendente',
+    date: '02/07/2021',
+    total: '9,96',
+  },
+  {
+    id: 2,
+    status: 'pendente',
+    date: '03/07/2021',
+    total: '20,99',
+  },
+  {
+    id: 3,
+    status: 'pendente',
+    date: '04/07/2021',
+    total: '39,90',
+  },
+  {
+    id: 1,
+    status: 'pendente',
+    date: '05/07/2021',
+    total: '15,25',
   },
 ];
 
@@ -23,13 +40,13 @@ const CustomerOrders = () => {
     if (!userData) return history.push('/login');
     setUser(userData);
     setOrders(MOCK_ORDERS);
-  });
+  }, [history]);
 
   return (
     <>
       <Navbar name={ user.name } />
       <h1>Pagina de pedidos do cliente</h1>
-      { orders.map((order) => <h2 key={ order.productId }>OrderCard</h2>) }
+      { orders.map((order) => <CustomerOrderCard key={ order.id } order={ order } />) }
     </>
   );
 };
