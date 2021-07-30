b8d56b32602d724ba99fbca8c51407507ac84016
Daniel <danieldsrd@gmail.com>
2021-07-30 16:26:28 -0300

    lint fix

--- front-end/src/context/LoginProvider.js
+++ front-end/src/context/LoginProvider.js
@@ -24,12 +24,6 @@ export function LoginProvider({ children }) {
     await request(API_LOGIN_URL({ email, password }));
   };
 
-  const roleConfig = {
-    customer: '/customer/products',
-    seller: '/seller/order',
-    administrator: '/admin/manage',
-  };
-
   const history = useHistory();
   if (response) {
     localStorage.setItem('user', JSON.stringify(response.data));
