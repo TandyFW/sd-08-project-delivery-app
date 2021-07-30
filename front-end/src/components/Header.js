467e6165ec14235294f2a1e01b61f2ff7f87e321
jnpefo <jnpefo@gmail.com>
2021-07-30 20:16:47 -0300

    Creat Header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
+const ITEM1 = 'customer_products__element-navbar-link-products';
+const ITEM2 = 'customer_products__element-navbar-link-orders';
+const NICK = 'customer_products__element-navbar-user-full-name';
+const SAIR = 'customer_products__element-navbar-link-logout';
 export default function Header() {
-  return (<header>HEADER AQUI</header>);
+  const history = useHistory();
+  const local = history.location.pathname;
+  if (local.search('customer')) {
+    return (
+      <header>
+        <button type="button" data-teste-id={ `${ITEM1}` }>PRODUTOS</button>
+        <button type="button" data-teste-id={ `${ITEM2}` }>MEUS PEDIDOS</button>
+        <h3 data-teste-id={ `${NICK}` }>NICK</h3>
+        <button type="button" data-teste-id={ `${SAIR}` }>SAIR</button>
+      </header>);
+  }
+  if (local.search('seller')) {
+    return (
+      <header>
+        <button type="button" data-teste-id={ `${ITEM1}` }>PEDIDOS</button>
+        <h3 data-teste-id={ `${NICK}` }>NICK</h3>
+        <button type="button" data-teste-id={ `${SAIR}` }>SAIR</button>
+      </header>);
+  }
+  if (local.search('admin')) {
+    return (
+      <header>
+        <button type="button" data-teste-id={ `${ITEM1}` }>GERENCIAR USUÁRIOS</button>
+        <h3 data-teste-id={ `${NICK}` }>NICK</h3>
+        <button type="button" data-teste-id={ `${SAIR}` }>SAIR</button>
+      </header>);
+  }
 }
