e315bbc49c4a51e6c918d70a464268da2604855f
Paulo Simões <paulohbsimoes@gmail.com>
2021-08-01 11:52:57 -0300

    feat(front): adjust component names in router imports

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Products from '../pages/Products';
-import Checkout from '../pages/Checkout';
+import Login from '../pages/common/Login';
+import Register from '../pages/common/Register';
+import Products from '../pages/customer/Products';
+import Checkout from '../pages/customer/Checkout';
 
 const Routes = () => (
   <BrowserRouter>
