2c2eec325ca1da13372adccc236e14e4009961af
Victor Miwa <victoryukimiwa@hotmail.com>
2021-08-02 15:54:19 -0300

    Adicionando o campo email no retorno da api de login

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -14,7 +14,7 @@ const findUser = async (email, password) => {
       };
     }
 
-    const user = { id: result.id, name: result.name, role: result.role };
+    const user = { id: result.id, email, name: result.name, role: result.role };
     const newToken = JWT(result);
     return { statusCode: 200, json: { validate: true, token: newToken, user } };
   } catch (err) {
