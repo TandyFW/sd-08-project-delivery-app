1e711fa3f4086a33f7fb2854eb6f94a6dfd3f2a1
hugobrr <hugobrr@gmail.com>
2021-08-03 20:29:43 -0300

    corrigndo erros do lint 3

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+// import PropTypes from 'prop-types';
+// import React from 'react';
 
 // function Homepage({ socket }) {
 //   // activates joinRoom function defined on the backend
