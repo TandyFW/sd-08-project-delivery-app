1a0dd442cca69bcc1cfc9936d98dc902a42f95f7
jnpefo <jnpefo@gmail.com>
2021-08-03 16:36:01 -0300

    Req 11, 12, 13, 14, 15 e 16

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import DeliveryAppContext from '../context/DeliveryAppContext';
 
 const ITEM1 = 'customer_products__element-navbar-link-products';
@@ -10,29 +10,40 @@ export default function Header() {
   const { user } = useContext(DeliveryAppContext);
   const history = useHistory();
   const local = history.location.pathname;
+
+  const clearStorage = () => {
+    localStorage.clear();
+  };
+
   if (local.search('customer')) {
     return (
       <header>
-        <button type="button" data-teste-id={ `${ITEM1}` }>PRODUTOS</button>
-        <button type="button" data-teste-id={ `${ITEM2}` }>MEUS PEDIDOS</button>
-        <h3 data-teste-id={ `${NICK}` }>{ user.name }</h3>
-        <button type="button" data-teste-id={ `${SAIR}` }>SAIR</button>
+        <button type="button" data-testid={ `${ITEM1}` }>PRODUTOS</button>
+        <button type="button" data-testid={ `${ITEM2}` }>MEUS PEDIDOS</button>
+        <h3 data-testid={ `${NICK}` }>{ user.name }</h3>
+        <button type="button" data-testid={ `${SAIR}` } onClick={ clearStorage }>
+          <Link to="/login">SAIR</Link>
+        </button>
       </header>);
   }
   if (local.search('seller')) {
     return (
       <header>
-        <button type="button" data-teste-id={ `${ITEM1}` }>PEDIDOS</button>
-        <h3 data-teste-id={ `${NICK}` }>NICK</h3>
-        <button type="button" data-teste-id={ `${SAIR}` }>SAIR</button>
+        <button type="button" data-testid={ `${ITEM1}` }>PEDIDOS</button>
+        <h3 data-testid={ `${NICK}` }>NICK</h3>
+        <button type="button" data-testid={ `${SAIR}` } onClick={ clearStorage }>
+          <Link to="/login">SAIR</Link>
+        </button>
       </header>);
   }
   if (local.search('admin')) {
     return (
       <header>
-        <button type="button" data-teste-id={ `${ITEM1}` }>GERENCIAR USUÁRIOS</button>
-        <h3 data-teste-id={ `${NICK}` }>NICK</h3>
-        <button type="button" data-teste-id={ `${SAIR}` }>SAIR</button>
+        <button type="button" data-testid={ `${ITEM1}` }>GERENCIAR USUÁRIOS</button>
+        <h3 data-testid={ `${NICK}` }>NICK</h3>
+        <button type="button" data-testid={ `${SAIR}` } onClick={ clearStorage }>
+          <Link to="/login">SAIR</Link>
+        </button>
       </header>);
   }
 }
