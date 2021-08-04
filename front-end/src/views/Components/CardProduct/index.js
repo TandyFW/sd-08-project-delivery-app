fc5af4be1ebd76d034a8ce6dbfbd568ecc67301f
Bruno Martins <bruno.martins217@gmail.com>
2021-08-04 18:15:41 -0300

    css

--- front-end/src/views/Components/CardProduct/index.js
+++ front-end/src/views/Components/CardProduct/index.js
@@ -44,43 +44,46 @@ const CardProduct = ({ product }) => {
       >
         {price.replace('.', ',')}
       </div>
-      <img
-        data-testid={ `customer_products__img-card-bg-image-${id}` }
-        src={ urlImage }
-        alt="produto"
-      />
-      <p
-        className="card_body"
-        data-testid={ `customer_products__element-card-title-${id}` }
-      >
-        {name}
-      </p>
-      <div className="card_foot">
-        <button
-          type="button"
-          onClick={ () => dec() }
-          data-testid={ `customer_products__button-card-rm-item-${id}` }
-        >
-          -
-        </button>
-        <input
-          type="text"
-          value={ quantity }
-          onChange={ (e) => {
-            if (e.target.value < 0) {
-              return;
-            }
-            setQuantity(Number(e.target.value));
-          } }
-          data-testid={ `customer_products__input-card-quantity-${id}` }
+      <div className="card_image">
+        <img
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+          src={ urlImage }
+          alt="produto"
         />
-        <button
-          type="button"
-          onClick={ () => inc() }
-          data-testid={ `customer_products__button-card-add-item-${id}` }
+      </div>
+      <div className="card_bottom">
+        <p
+          data-testid={ `customer_products__element-card-title-${id}` }
         >
-          +
-        </button>
+          {name}
+        </p>
+        <div className="card_foot">
+          <button
+            type="button"
+            onClick={ () => dec() }
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+          >
+            -
+          </button>
+          <input
+            type="text"
+            value={ quantity }
+            onChange={ (e) => {
+              if (e.target.value < 0) {
+                return;
+              }
+              setQuantity(Number(e.target.value));
+            } }
+            data-testid={ `customer_products__input-card-quantity-${id}` }
+          />
+          <button
+            type="button"
+            onClick={ () => inc() }
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
