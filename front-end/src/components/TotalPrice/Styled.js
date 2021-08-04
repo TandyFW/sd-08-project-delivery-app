0b021f1a2fe072f4bcad2fb9586208804daba843
emassaki <massakihirayama@gmail.com>
2021-08-04 18:36:56 -0300

    Pequenas alteraçoes

--- front-end/src/components/TotalPrice/Styled.js
+++ front-end/src/components/TotalPrice/Styled.js
@@ -5,23 +5,24 @@ export const PriceTag = styled.div`
   bottom: 0px;
   right: 20px;
   font-size: 15px;
-  width:350px;
-  background-color: #036B52;
+  width: 350px;
+  background-color: #036b52;
   cursor: pointer;
-border-top-left-radius:10px;
-border-top-right-radius:10px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
   font-weight: 600;
 `;
+
 export const Container = styled.div`
-height:50px
-display:flex;
-justify-content:space-evenly;
-align-items:center;
-width:100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
 `;
+
 export const Container1 = styled.div`
-width: 100%;
+  width: 100%;
 
-
-display: ${({ show }) => (show ? 'block' : 'none')};
+  display: ${({ show }) => (show ? 'block' : 'none')};
 `;
