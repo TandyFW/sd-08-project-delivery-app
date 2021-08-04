84cb4d2195688163fe84c5663dfd73639efe001b
Daniel <marcio.daniel@msn.com>
2021-08-04 17:19:01 -0300

    refatorando sockett

--- back-end/src/api/socket.js
+++ back-end/src/api/socket.js
@@ -1,52 +1,43 @@
-// function socket2(socket, listUser) {
-//   socket.on('make', (buyData) => {
-//     const { sellerId, userId, state } = buyData;
-//     console.log('vendedor ', sellerId, ' usuario ', userId, ' estado ', state);
-//     console.log(listUser, listUser[sellerId], listUser[userId]);
-//     switch (state) {
-//       case 'Pendente':
-//         console.log('Pendente', listUser[sellerId]);
-//         socket.to(listUser[sellerId]).emit('state', 'Pendente');
-//         break;
-//       case 'Preparando':
-//         console.log('Preparando', listUser[userId]);
-//         socket.to(listUser[userId]).emit('state', 'Preparando');
-//         break;
-//       case 'Em Trânsito':
-//         console.log('Em Trânsito', listUser[userId]);
-//         socket.to(listUser[userId]).emit('state', 'Em Trânsito');
-//         break;
-//       case 'Entregue':
-//         console.log('Entregue', listUser[sellerId]);
-//         socket.to(listUser[sellerId]).emit('state', 'Entregue');
-//         break;
-//       default:
-//         console.log('erro', listUser[sellerId]);
-//         socket.to(listUser[sellerId]).emit('state', 'erro');
-//         socket.to(listUser[userId]).emit('state', 'erro');
-//         break;
-//     }
-//   });
-// }
+const emTransito = 'Em Trânsito';
 
-// const listUser = {};
+function socket2(socket, listUser) {
+  socket.on('make', ({ sellerId, userId, state }) => {
+    console.log('vendedor ', sellerId, ' usuario ', userId, ' estado ', state);
+    console.log(listUser, listUser[sellerId], listUser[userId]);
+    switch (state) {
+      case 'Pendente': console.log('Pendente', listUser[sellerId]);
+        return socket.to(listUser[sellerId]).emit('state', 'Pendente');
+      case 'Preparando': console.log('Preparando', listUser[userId]);
+        return socket.to(listUser[userId]).emit('state', 'Preparando');
+      case emTransito: console.log(emTransito, listUser[userId]);
+        return socket.to(listUser[userId]).emit('state', emTransito);
+      case 'Entregue': console.log('Entregue', listUser[sellerId]);
+        return socket.to(listUser[sellerId]).emit('state', 'Entregue');
+      default:
+        console.log('erro', listUser[sellerId]);
+        socket.to(listUser[sellerId]).emit('state', 'erro');
+        return socket.to(listUser[userId]).emit('state', 'erro');
+    }
+  });
+}
+
+const listUser = {};
 
 const delivery = (io) => {
-    console.log('oi');
   io.on('connection', (socket) => {
     console.log(`Usuário conectado. ID: ${socket.id} `);
-    // socket.on('ping', () => {
-    //   console.log(`${socket.id} emitiu um ping!`);
-    // });
-    // socket.on('hello', (id) => {
-    //   console.log('O usuario ', id, 'chegou');
-    //   listUser[id] = socket.id;
-    // });
-    // socket2(socket, listUser);
-    // socket.on('disconnect', () => {});
+    socket.on('ping', () => {
+      console.log(`${socket.id} emitiu um ping!`);
+    });
+    socket.on('hello', (id) => {
+      console.log('O Usuário', id, 'chegou');
+      listUser[id] = socket.id;
+    });
+    socket2(socket, listUser);
+    socket.on('disconnect', () => {});
   });
 };
 
 module.exports = {
-    delivery,
+  delivery,
 };
