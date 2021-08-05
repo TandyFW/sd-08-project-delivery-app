6b74e5ea45dde68b3c31c92ec65e79b5287e76d0
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-08-05 17:46:57 -0300

    corrige customer orders router

--- back-end/src/routers/CustomerOrdersRouter.js
+++ back-end/src/routers/CustomerOrdersRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const AuthController = require('../controllers/AuthController');
-const { getAllUserOrders, getOrderById, updateOrderStatus } = require('../controllers/CustomerOrdersController');
+const {
+  getAllUserOrders,
+  getOrderById,
+  updateOrderStatus } = require('../controllers/CustomerOrdersController');
 
 const CustomerOrdersRouter = express.Router();
 
 CustomerOrdersRouter.get('/customer/orders', AuthController, getAllUserOrders);
-CustomerOrdersRouter
-  .put('/customer/orders/:id', AuthController, updateOrderStatus);
+CustomerOrdersRouter.put('/customer/orders/:id', AuthController, updateOrderStatus);
 CustomerOrdersRouter.get('/customer/orders/:id', AuthController, getOrderById);
 
 module.exports = CustomerOrdersRouter;
