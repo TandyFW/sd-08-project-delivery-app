9fbb5e0ebe159c86fb30afaa8e53bd6f18ccb310
Fabio Ferreira <fabio35547@gmail.com>
2021-08-08 14:25:50 -0300

    Inclusao do arquivo styled em OrderList

---
+++ front-end/src/components/OrderList/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const OrderListBody = styled.div`align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 40px;
+`;
+
+export default OrderListBody;
